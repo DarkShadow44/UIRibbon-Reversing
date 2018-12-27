db29484d9bf44a5c8f2978381ce490e540c95769
Fabian Maurer <dark.shadow4@web.de>
2018-12-27 01:18:24 +0100

    move files

---
+++ link.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ln -s /home/fabian/Test/UICC/kaitai_struct/tests/KaitaiCCompiler /dev/shm/ramdisk/KaitaiCCompiler
+ln -s /home/fabian/Test/UICC/kaitai_struct/tests/UIRibbon/parser_uiribbon.c /dev/shm/ramdisk/parser_uiribbon.c
+ln -s /home/fabian/Test/UICC/kaitai_struct/tests/UIRibbon/parser_uiribbon.h /dev/shm/ramdisk/parser_uiribbon.h
+ln -s /home/fabian/Test/UICC/kaitai_struct/new.ksy /dev/shm/ramdisk/new.ksy
--- test/link.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ln -s /home/fabian/Test/UICC/kaitai_struct/test/tests/KaitaiCCompiler /dev/shm/ramdisk/KaitaiCCompiler
-ln -s /home/fabian/Test/UICC/kaitai_struct/test/tests/UIRibbon/parser_uiribbon.c /dev/shm/ramdisk/parser_uiribbon.c
-ln -s /home/fabian/Test/UICC/kaitai_struct/test/tests/UIRibbon/parser_uiribbon.h /dev/shm/ramdisk/parser_uiribbon.h
-ln -s /home/fabian/Test/UICC/kaitai_struct/test/new.ksy /dev/shm/ramdisk/new.ksy
